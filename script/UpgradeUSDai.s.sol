6961fb89d87d5e4d3e12e6c25e9507917edf6243
denalimarsh <denalimarsh@gmail.com>
2026-04-26 17:46:47 +0200

    fix UpgradeUSDai wiring for v1.5 migration

--- script/UpgradeUSDai.s.sol
+++ script/UpgradeUSDai.s.sol
@@ -12,10 +12,21 @@ import {Deployer} from "./utils/Deployer.s.sol";
 contract UpgradeUSDai is Deployer {
     function run() public broadcast useDeployment returns (address) {
         if (_deployment.swapAdapter == address(0)) revert MissingDependency();
+        if (_deployment.stakedUSDai == address(0)) revert MissingDependency();
+        if (_deployment.swapFacility == address(0)) revert MissingDependency();
 
-        // Deploy USDai implemetation
-        USDai USDaiImpl = new USDai(_deployment.swapAdapter, _deployment.baseYieldEscrow, _deployment.stakedUSDai);
+        // Deploy USDai implementation
+        // TODO(R1): If routing option R1 is selected, USDai's constructor will likely
+        // drop the `swapAdapter` argument. Update this call to match.
+        USDai USDaiImpl = new USDai(
+            _deployment.swapAdapter,
+            _deployment.stakedUSDai, // baseYieldRecipient_
+            _deployment.swapFacility // swapFacility_
+        );
         console.log("USDai implementation", address(USDaiImpl));
+
+        /* Build initializer calldata for the v1.5 PYUSD -> MultiMint migration */
+        bytes memory initCalldata = abi.encodeCall(USDai.initializeV1_5, ());
 
         /* Lookup proxy admin */
         address proxyAdmin = address(uint160(uint256(vm.load(_deployment.USDai, ERC1967Utils.ADMIN_SLOT))));
@@ -23,7 +34,7 @@ contract UpgradeUSDai is Deployer {
         if (Ownable(proxyAdmin).owner() == msg.sender) {
             /* Upgrade Proxy */
             ProxyAdmin(proxyAdmin).upgradeAndCall(
-                ITransparentUpgradeableProxy(_deployment.USDai), address(USDaiImpl), ""
+                ITransparentUpgradeableProxy(_deployment.USDai), address(USDaiImpl), initCalldata
             );
             console.log("Upgraded proxy %s implementation to: %s\n", _deployment.USDai, address(USDaiImpl));
         } else {
@@ -35,7 +46,7 @@ contract UpgradeUSDai is Deployer {
                     ProxyAdmin.upgradeAndCall.selector,
                     ITransparentUpgradeableProxy(_deployment.USDai),
                     address(USDaiImpl),
-                    ""
+                    initCalldata
                 )
             );
         }
